6c716096054b73db465059b7bc3ecb712b26daa0
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-19 20:38:50 -0600

    step 6, part 2 completed.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const massive = require('massive')
 const authCtrl = require('./controllers/authController')
 const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware')
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 
 const app = express()
@@ -22,7 +23,7 @@ app.get('/auth/logout', authCtrl.logout)
 
 // Treasure
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
-app.get('/api/treasure/user', treasureCtrl.getUserTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db)
