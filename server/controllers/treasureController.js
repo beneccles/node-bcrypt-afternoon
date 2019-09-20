17e934d3334cc8187c6390d0b3e5b789963b99c1
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-19 21:02:20 -0600

    step 7 part 1 complete

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -8,5 +8,13 @@ module.exports = {
         const db = req.app.get('db')
         const treasure = await db.get_user_treasure(req.session.user.id)
         res.status(200).send(treasure)
+    },
+    addUserTreasure: async (req, res) => {
+        const db = req.app.get('db')
+        const { treasureURL } = req.body
+        const { id } = req.session.user
+        const userTreasure = await db.add_user_treasure(treasureURL, id)
+        res.status(200).send(userTreasure)
+
     }
 }
--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.get('/auth/logout', authCtrl.logout)
 // Treasure
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
 app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db)
